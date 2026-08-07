0a29f888cd5fc492f4412488c11d668a76632b4f
Prashast Rawat <prashastrawat123@gmail.com>
2026-08-07 21:33:08 +0530

    Add Service model and controller (CRUD, not yet route-protected)

---
+++ server/models/Service.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+
+const serviceSchema = new mongoose.Schema(
+    {
+        title:{
+            type: String,
+            required: [true, "Title is required"],
+            trim: true,
+        },
+        description:{
+            type:String,
+            required:[true, "Description is required"]
+        },
+        image:{
+            url:{
+                type: String,
+                required:[true, "Image is required"],
+            },
+            publicId:{
+                type:String,
+                required: true,
+            },
+        },
+        features:{
+            type:[String],
+            default:[],
+        },
+        priceStartingFrom:{
+            type: Number,
+            required: [true, "Starting price is required"]
+        },
+        isActive:{
+            type:Boolean,
+            default: true,
+        },
+    },
+    {timestamps:true}
+);
+
+const Service = mongoose.model("Service", serviceSchema)
+
+export default Service
